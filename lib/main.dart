91e2dc19dd156a4508fbac41576881144a7066e3
mohamadazafri <azafri9@gmail.com>
2025-03-28 13:22:30 +0800

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -33,6 +33,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Homestay Booking',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
